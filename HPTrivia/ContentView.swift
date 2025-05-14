9d1647c87d238366d089b661e8dfee73f6df74e4
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-05-13 18:46:35 -0700

    Adding Audio and Animation to Title Screen

--- HPTrivia/ContentView.swift
+++ HPTrivia/ContentView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var scalePlayButton = false
+    @State private var moveBackgroundImage = false
+    
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -15,6 +21,12 @@ struct ContentView: View {
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
                     .padding(.top, 3)
+                    .offset(x: moveBackgroundImage ? geo.size.width/1.1 : -geo.size.width/1.1)
+                    .onAppear{
+                        withAnimation(.linear(duration: 60).repeatForever()) {
+                            moveBackgroundImage.toggle()
+                        }
+                    }
                 
                 VStack {
                     VStack {
@@ -75,6 +87,12 @@ struct ContentView: View {
                                 .cornerRadius(8)
                                 .shadow(radius: 5)
                         }
+                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                        .onAppear{
+                            withAnimation(.easeInOut(duration: 1.2).repeatForever()) {
+                                scalePlayButton.toggle()
+                            }
+                        }
                         
                         Spacer()
                         
@@ -97,6 +115,17 @@ struct ContentView: View {
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear {
+                playAudio()
+        }
+    }
+    
+    private func playAudio() {
+        let sound = Bundle.main.path(forResource: "magic-in-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
+        
     }
 }
 
