442c441d25172c078fa622b0213ac43eedb8e75d
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-05-13 17:20:28 -0700

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Aakash Ambodkar on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
