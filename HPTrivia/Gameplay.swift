6905de1578f39202a74315f12f40fcceb74d15d0
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-05-16 19:44:54 -0700

    Built Game Screen & attached it to HomeScreen

--- HPTrivia/Gameplay.swift
+++ HPTrivia/Gameplay.swift
@@ -14,7 +14,7 @@ struct Gameplay: View {
                 Image("hogwarts")
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height * 1.05)
-                    .overlay(Rectangle().foregroundColor(.black.opacity(0.5)))
+                    .overlay(Rectangle().foregroundColor(.black.opacity(0.75)))
                 
                 VStack {
                     HStack {
@@ -26,9 +26,57 @@ struct Gameplay: View {
                         
                         Spacer()
                         
-                        Text("Score: 0")
+                        Text("Score: 28")
                     }
                     .padding()
+                    .padding(.vertical, 32)
+                    
+                    Text("Who is Harry Potter?") // Sample Question
+                        .font(.custom(Constants.hpFont, size: 50))
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "questionmark.app.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100)
+                            .foregroundColor(.cyan)
+                            .rotationEffect(.degrees(-11))
+                            .padding()
+                            .padding(.leading, 20)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "book.closed")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50)
+                            .foregroundColor(.black)
+                            .frame(width: 100, height: 100)
+                            .background(.cyan)
+                            .cornerRadius(20)
+                            .rotationEffect(.degrees(11))
+                            .padding()
+                            .padding(.trailing, 20)
+                    }
+                    .padding(.bottom)
+                    
+                    LazyVGrid(columns: [GridItem(), GridItem()]) {
+                        ForEach(1..<5) { i in
+                                Text("Answer \(i)")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
+                                .padding(10)
+                                .frame(width: geo.size.width/2.15, height: 80)
+                                .background(.green.opacity(0.5))
+                                .cornerRadius(25)
+                        }
+                    }
+                    
+                    Spacer()
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
                 .foregroundColor(.white)
