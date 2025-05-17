4439903a75a3b0c2a06cc1adb2c4978c84e290c0
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-05-16 20:10:01 -0700

    Added transitions to gameplay screen

--- HPTrivia/Gameplay.swift
+++ HPTrivia/Gameplay.swift
@@ -8,82 +8,115 @@
 import SwiftUI
 
 struct Gameplay: View {
+    @State private var animateViewsIn = false
+    
     var body: some View {
         GeometryReader { geo in
-            ZStack {
-                Image("hogwarts")
-                    .resizable()
-                    .frame(width: geo.size.width * 3, height: geo.size.height * 1.05)
-                    .overlay(Rectangle().foregroundColor(.black.opacity(0.75)))
-                
-                VStack {
-                    HStack {
-                        Button("End Game") {
-                            // TODO: End Game
-                        }
-                        .buttonStyle(.borderedProminent)
-                        .tint(.red.opacity(0.8))
+                ZStack {
+                    Image("hogwarts")
+                        .resizable()
+                        .frame(width: geo.size.width * 3, height: geo.size.height * 1.05)
+                        .overlay(Rectangle().foregroundColor(.black.opacity(0.75)))
                         
-                        Spacer()
-                        
-                        Text("Score: 28")
-                    }
-                    .padding()
-                    .padding(.vertical, 32)
                     
-                    Text("Who is Harry Potter?") // Sample Question
-                        .font(.custom(Constants.hpFont, size: 50))
-                        .multilineTextAlignment(.center)
+                    VStack {
+                        // MARK: Controls
+                        HStack {
+                            Button("End Game") {
+                                // TODO: End Game
+                            }
+                            .buttonStyle(.borderedProminent)
+                            .tint(.red.opacity(0.8))
+                            
+                            Spacer()
+                            
+                            Text("Score: 28")
+                        }
                         .padding()
-                    
-                    Spacer()
-                    
-                    HStack {
-                        Image(systemName: "questionmark.app.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100)
-                            .foregroundColor(.cyan)
-                            .rotationEffect(.degrees(-11))
-                            .padding()
-                            .padding(.leading, 20)
+                        .padding(.vertical, 50)
+                        
+                        // MARK: Questions
+                        VStack {
+                            if animateViewsIn{
+                                Text("Who is Harry Potter?") // Sample Question
+                                    .font(.custom(Constants.hpFont, size: 50))
+                                    .multilineTextAlignment(.center)
+                                    .padding()
+                                    .transition(.scale)
+                            }
+                        }
+                        .animation(.easeInOut(duration: 2), value: animateViewsIn)
                         
                         Spacer()
-                        
-                        Image(systemName: "book.closed")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50)
-                            .foregroundColor(.black)
-                            .frame(width: 100, height: 100)
-                            .background(.cyan)
-                            .cornerRadius(20)
-                            .rotationEffect(.degrees(11))
-                            .padding()
-                            .padding(.trailing, 20)
-                    }
-                    .padding(.bottom)
-                    
-                    LazyVGrid(columns: [GridItem(), GridItem()]) {
-                        ForEach(1..<5) { i in
-                                Text("Answer \(i)")
-                                .minimumScaleFactor(0.5)
-                                .multilineTextAlignment(.center)
-                                .padding(10)
-                                .frame(width: geo.size.width/2.15, height: 80)
-                                .background(.green.opacity(0.5))
-                                .cornerRadius(25)
+                        // MARK: Hints
+                        HStack {
+                            VStack {
+                                if animateViewsIn{
+                                    Image(systemName: "questionmark.app.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100)
+                                        .foregroundColor(.cyan)
+                                        .rotationEffect(.degrees(-11))
+                                        .padding()
+                                        .padding(.leading, 20)
+                                        .transition(.offset(x: -geo.size.width/2))
+                                }
+                            }
+                            .animation(.easeOut(duration:1.7).delay(2), value: animateViewsIn)
+                            
+                            Spacer()
+                            
+                            VStack {
+                                if animateViewsIn{
+                                    Image(systemName: "book.closed")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 50)
+                                        .foregroundColor(.black)
+                                        .frame(width: 100, height: 100)
+                                        .background(.cyan)
+                                        .cornerRadius(20)
+                                        .rotationEffect(.degrees(11))
+                                        .padding()
+                                        .padding(.trailing, 20)
+                                        .transition(.offset(x: geo.size.width/2))
+                                }
+                            }
+                            .animation(.easeOut(duration:1.7).delay(2), value: animateViewsIn)
                         }
+                        .padding(.bottom)
+                        
+                        // MARK: Answers
+                        LazyVGrid(columns: [GridItem(), GridItem()]) {
+                            ForEach(1..<5) { i in
+                                VStack {
+                                    if animateViewsIn{
+                                        Text("Answer \(i)")
+                                            .minimumScaleFactor(0.5)
+                                            .multilineTextAlignment(.center)
+                                            .padding(10)
+                                            .frame(width: geo.size.width/2.15, height: 80)
+                                            .background(.green.opacity(0.5))
+                                            .cornerRadius(25)
+                                            .transition(.scale)
+                                    }
+                                }
+                                .animation(.easeOut(duration: 1).delay(1.4), value: animateViewsIn)
+                            }
+                        }
+                        
+                        Spacer()
                     }
-                    
-                    Spacer()
+                    .frame(width: geo.size.width, height: geo.size.height)
+                    .foregroundColor(.white)
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
-                .foregroundColor(.white)
             }
-            .frame(width: geo.size.width, height: geo.size.height)
-        }
         .ignoresSafeArea()
+        .onAppear {
+            animateViewsIn = true
+        }
     }
 }
 
