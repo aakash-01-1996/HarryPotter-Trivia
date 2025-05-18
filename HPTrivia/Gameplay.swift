f9c8699ff64a6ec3c429d11d30dea714bf6aec18
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-05-17 20:17:18 -0700

    UserInteraction - reveal Hint and Book

--- HPTrivia/Gameplay.swift
+++ HPTrivia/Gameplay.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct Gameplay: View {
+    @Environment(\.dismiss) private var dismiss
     @State private var animateViewsIn = false
     @State private var correctAnswer = false
     @State private var hintWiggle = false
     
     @State private var scaleNextButton = false
     @State private var movePointsToScore = false
+    
+    @State private var revealHint = false
+    @State private var revealBook = false
     
     var body: some View {
         GeometryReader { geo in
@@ -27,7 +31,7 @@ struct Gameplay: View {
                         // MARK: Controls
                         HStack {
                             Button("Nox") {
-                                // TODO: End Game
+                                dismiss()
                             }
                             .buttonStyle(.borderedProminent)
                             .tint(.red.opacity(0.8))
@@ -74,6 +78,23 @@ struct Gameplay: View {
                                                 hintWiggle = true
                                             }
                                         }
+                                        .onTapGesture {
+                                            withAnimation(.easeOut(duration: 1)) {
+                                                revealHint = true
+                                            }
+                                        }
+                                        .rotation3DEffect(.degrees(revealHint ? 1440 : 0), axis: (x: 0, y: 1, z: 0))
+                                        .scaleEffect(revealHint ? 5: 1)
+                                        .opacity(revealHint ? 0 : 1)
+                                        .offset(x: revealHint ? geo.size.width/2 : 0)
+                                        .overlay(
+                                            Text("The Boy who Lived")
+                                                .padding(.leading, 33)
+                                                .minimumScaleFactor(0.5)
+                                                .multilineTextAlignment(.center)
+                                                .opacity(revealHint ? 1 : 0)
+                                                .scaleEffect(revealHint ? 1.33 : 1)
+                                        )
                                 }
                             }
                             .animation(.easeOut(duration:1.7).delay(2), value: animateViewsIn)
@@ -99,6 +120,23 @@ struct Gameplay: View {
                                                 hintWiggle = true
                                             }
                                         }
+                                        .onTapGesture {
+                                            withAnimation(.easeOut(duration: 1)) {
+                                                revealBook = true
+                                            }
+                                        }
+                                        .rotation3DEffect(.degrees(revealBook ? 1440 : 0), axis: (x: 0, y: 1, z: 0))
+                                        .scaleEffect(revealBook ? 5: 1)
+                                        .opacity(revealBook ? 0 : 1)
+                                        .offset(x: revealBook ? -geo.size.width/2 : 0)
+                                        .overlay(
+                                            Image("hp1")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .padding(.trailing, 33)
+                                                .opacity(revealBook ? 1 : 0)
+                                                .scaleEffect(revealBook ? 1.33 : 1)
+                                        )
                                 }
                             }
                             .animation(.easeOut(duration:1.7).delay(2), value: animateViewsIn)
@@ -209,8 +247,8 @@ struct Gameplay: View {
             }
         .ignoresSafeArea()
         .onAppear {
-//            animateViewsIn = true
-            correctAnswer = true
+            animateViewsIn = true
+//            correctAnswer = true
         }
     }
 }
