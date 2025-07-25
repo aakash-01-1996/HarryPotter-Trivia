849870d7912b6acafcb221376faabe5d75e91351
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-07-25 14:43:33 -0700

    fix: Chage book status

--- HPTrivia/Views/SelectBooks.swift
+++ HPTrivia/Views/SelectBooks.swift
@@ -11,6 +11,7 @@ struct SelectBooks: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(Game.self) private var game
     
+    @State private var showTempAlert = false
     var body: some View {
         ZStack {
             InfoBackgroundImage()
@@ -39,19 +40,49 @@ struct SelectBooks: View {
                                         .padding(3)
                                     
                                 }
+                                .onTapGesture{
+                                    game.bookQuestions.changeStatus(of: book.id, to: .inactive)
+                                }
+                                
                             } else if book.status == .inactive {
-                                ZStack {
+                                ZStack(alignment: .bottomTrailing) {
                                     Image(book.image)
                                         .resizable()
                                         .scaledToFit()
                                         .shadow(radius: 7)
+                                        .overlay{
+                                            Rectangle().opacity(0.37)
+                                        }
+                                    
+                                    Image(systemName: "circle")
+                                        .font(.largeTitle)
+                                        .imageScale(.medium)
+                                        .foregroundStyle(.green.opacity(0.5))
+                                        .padding(3)
                                 }
+                                .onTapGesture{
+                                    game.bookQuestions.changeStatus(of: book.id, to: .active)
+                                }
+                                
                             } else {
                                 ZStack {
                                     Image(book.image)
                                         .resizable()
                                         .scaledToFit()
                                         .shadow(radius: 7)
+                                        .overlay{
+                                            Rectangle().opacity(0.75)
+                                        }
+                                    
+                                    Image(systemName: "lock.fill")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .shadow(color: .white, radius: 2)
+                                }
+                                .onTapGesture{
+                                    // in-app purchases
+                                    showTempAlert.toggle()
+                                    game.bookQuestions.changeStatus(of: book.id, to: .active)
                                 }
                             }
                         }
@@ -63,6 +94,8 @@ struct SelectBooks: View {
                 }
                 .donebutton()
             }
+        }
+        .alert("You purchased this book!", isPresented: $showTempAlert){
         }
     }
 }
